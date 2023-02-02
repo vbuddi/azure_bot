f91da42ecb0d3507acad0dfd5743444567b1b19c
vbuddi <vbuddi@users.noreply.github.com>
2023-02-01 22:37:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -86,6 +86,7 @@ def init_func(argv):
 
 if __name__ == "__main__":
     APP = init_func(None)
+    
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
